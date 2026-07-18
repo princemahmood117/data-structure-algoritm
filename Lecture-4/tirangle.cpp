7bf4bc2d08875c1ce5e37ccc5025323e135cfb53
iftekhar <princemahmood117@gmail.com>
2026-07-18 17:13:26 +0600

    number triangle

--- Lecture-4/tirangle.cpp
+++ Lecture-4/tirangle.cpp
@@ -7,15 +7,31 @@ int main() {
 
     for(int i = 0; i < n; i++) {
 
-        for(int j = 0; j < i; j++) {
+        for(int j = 0; j <= i; j++) {
             // number of stars depends on i
             cout << '*';
         }
         cout << endl;
     }
-
-
     return 0;
 }
 
 
+
+//  anoher logic
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//     int n = 6;
+
+//     for(int i = 0; i < n; i++){ 
+//         for(int j = 0; j < i+1; j++) {
+//             cout << "*";
+//         };
+//         cout << endl;
+//     }; 
+//     return 0;
+// }
