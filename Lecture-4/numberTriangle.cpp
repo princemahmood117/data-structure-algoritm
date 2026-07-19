2b8c1657a8c55ba1dccfc37b92d4c390d7665728
iftekhar <princemahmood117@gmail.com>
2026-07-19 16:23:49 +0600

    hollow Triangle

--- Lecture-4/numberTriangle.cpp
+++ Lecture-4/numberTriangle.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+    
+//     int n = 5;
+
+//     for(int i=0; i<n; i++){
+//         for(int j=1; j<=i; j++ ) {
+//             cout << i;
+//         };
+
+//         cout << endl;
+//     }
+//     return 0;
+
+//     /*
+//     Output:
+//     1
+//     22
+//     333
+//     4444
+//     */
+// }
+
+
+
+
+#include <iostream>
+using namespace std;
+
 int main() {
     
     int n = 5;
@@ -12,7 +40,21 @@ int main() {
 
         cout << endl;
     }
-
-
     return 0;
+
+    /*
+    Output:
+    1
+    12
+    123
+    1234
+    */
 }
+
+
+
+
+
+
+
+
--- Lecture-4/tempCodeRunnerFile.cpp
+++ Lecture-4/tempCodeRunnerFile.cpp
@@ -1,3 +1,14 @@
-    // for(int j = 0; j < n; j++){  
-        //     cout << "**" << endl;
-        // }
+int main() {
+    
+    int n = 5;
+
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=i; j++ ) {
+            cout << i;
+        };
+
+        cout << endl;
+    }
+
+    return 0;
+}
