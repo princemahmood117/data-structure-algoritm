83248af9c781337cf60b62cd38f184435839907b
iftekhar <princemahmood117@gmail.com>
2026-08-01 23:21:18 +0600

    array length

--- Lecture-7/array.cpp
+++ Lecture-7/array.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main() {
     
-    int ar[4] = [1, 2, 3, 4];
-    cout << ar;
+    int ar[] = {1, 2, 3, 4, 5};
+    
+    int size = sizeof(ar);
+    cout <<  "size of array is " << size <<  endl;
+
+    int singleSize = sizeof(ar[0]);
+    cout << "Size of any Single element " << singleSize << endl;
+
+    // size = 16 (1 int value = 4 byte, 5 int value = 20 byte) / singleElement value = (1 int = 4 byte) = 20 / 4 = 5 elements
+    int length = size / singleSize;   
+
+    cout << "Length of the array is " << length << endl;
+    
+
+    for(int i = 0; i < length; i++) {
+        cout << ar[i] << " ";
+    }
+   
     return 0;
 }
