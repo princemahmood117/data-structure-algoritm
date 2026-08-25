9eaa81d2df7a5b6922dc073030be990dbad00b43
iftekhar <princemahmood117@gmail.com>
2026-08-25 23:52:38 +0600

    moore algo

---
+++ Lecture-11/mooresAlgo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int mooreAlgo(vector<int> arr) {
+
+    int n = arr.size();
+    
+};
+
+
+
+int main() {
+    
+
+    // moore's algo do not need to sort the array
+
+    vector<int> arr = {1,2,2,1,1};
+    return 0;
+}
