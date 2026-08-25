05b874ec3edbe9ee57d2e346c219224674619bdd
iftekhar <princemahmood117@gmail.com>
2026-08-26 00:06:25 +0600

    completed moores algorithm

--- Lecture-11/mooresAlgo.cpp
+++ Lecture-11/mooresAlgo.cpp
@@ -5,7 +5,25 @@ using namespace std;
 int mooreAlgo(vector<int> arr) {
 
     int n = arr.size();
-    
+
+    int count = 0;
+    int ans = 0;
+
+    for(int i = 1; i < n; i++) {
+
+        if(count == 0) {
+            ans = arr[i];
+        };
+        if(ans = arr[i]) {
+            count = count + 1;
+        }
+        else {
+            count = count - 1;
+        }
+        
+    }
+
+    return ans;
 };
 
 
@@ -15,6 +33,10 @@ int main() {
 
     // moore's algo do not need to sort the array
 
-    vector<int> arr = {1,2,2,1,1};
+    vector<int> arr = {1,2,2,1,1,2,3,2};
+
+    int result = mooreAlgo(arr);
+    cout << "the element maximum time occured : " << result;
+
     return 0;
 }
