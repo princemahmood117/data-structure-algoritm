062887537945b33b56c930a5bc9fcb4a51c1c6a8
iftekhar <princemahmood117@gmail.com>
2026-09-05 23:42:37 +0600

    power of x

---
+++ Lecture-13/power.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+double myPower(double x, int n)
+{
+
+    long binaryForm = n;
+    double ans = 1;
+
+    if (n < 0)
+    {
+        x = 1 / x;
+        binaryForm = -binaryForm;
+    }
+
+    while (binaryForm > 0)
+    {
+        if (binaryForm % 2 == 1)
+        {
+            ans = ans * x;
+        }
+
+        x = x * x;
+        binaryForm = binaryForm / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+double result = myPower(10, 2);
+    cout << result;
+
+    return 0;
+}
